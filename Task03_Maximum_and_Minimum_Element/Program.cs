b73ae480228bd6b1ac30e34361a2ffe0c1e09569
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-26 23:11:38 +0200

    Add files via upload

---
+++ Task03_Maximum_and_Minimum_Element/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task03_Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int comandNumber = int.Parse(Console.ReadLine());
+
+            Stack<int> stackNumbers = new Stack<int>();
+            for (int i = 1; i <= comandNumber; i++)
+            {
+                string[] rawComand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string typeComand = rawComand[0];
+
+                switch (typeComand)
+                {
+                    case "1":
+                        stackNumbers.Push(int.Parse(rawComand[1]));
+                        break;
+
+                    case "2":
+                        if(stackNumbers.Count > 0)
+                        {
+                            stackNumbers.Pop();
+                        }
+                        break;
+
+                    case "3":
+                        if (stackNumbers.Any())
+                        {
+                            Console.WriteLine(stackNumbers.Max());
+                        }
+                        break;
+
+                    case "4":
+
+                        if (stackNumbers.Any())
+                        {
+                            Console.WriteLine(stackNumbers.Min());
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", stackNumbers));
+        }
+    }
+}
