c442b66aa40a6c2bed6183ae9ed5be3a858cc787
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-26 23:18:33 +0200

    Add files via upload

---
+++ Task08_Balanced_Parenthesis_2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task08_Balanced_Parenthesis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequenceParentheses = Console.ReadLine();
+
+            Stack<char> parentheses = new Stack<char>();
+            bool isBalanced = true;
+
+            foreach (char element in sequenceParentheses)
+            {
+                if(element == '(' || element == '{' || element == '[')
+                {
+                    parentheses.Push(element);
+                }
+
+                else if(!parentheses.Any())
+                {
+                    isBalanced = false;
+                    break;
+                   
+                }
+
+                else
+                {
+                    switch (element)
+                    {
+                        case ')':
+                            if(parentheses.Pop() != '(')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+
+                        case '}':
+                            if (parentheses.Pop() != '{')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+
+                        case ']':
+                            if (parentheses.Pop() != '[')
+                            {
+                                isBalanced = false;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if(!isBalanced)
+                {
+                    break;
+                }
+            }
+
+            if(parentheses.Any())
+            {
+                isBalanced = false;
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
