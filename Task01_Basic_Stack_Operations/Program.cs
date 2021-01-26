97c2a8e40fad4a2c70c72b7effe2d09f8bdff176
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-26 23:09:32 +0200

    Add files via upload

---
+++ Task01_Basic_Stack_Operations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task01_Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rawData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int putNumber = rawData[0];
+            int exitNumber = rawData[1];
+            int lookForNumber = rawData[2];
+
+            int[] rawNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> stackNumbers = new Stack<int>();
+            foreach (int elemet in rawNumbers)
+            {
+                stackNumbers.Push(elemet);
+            }
+
+            for (int i = 1; i <= exitNumber; i++)
+            {
+                stackNumbers.Pop();
+            }
+
+            //bool isLookNumber = false;
+            if(stackNumbers.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            
+            else if(stackNumbers.Contains(lookForNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stackNumbers.Min());
+            }
+        }
+    }
+}
