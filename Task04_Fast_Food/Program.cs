aae82066c4cb0774f2b5c6805ef4a497dbfd7a85
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-26 23:12:38 +0200

    Add files via upload

---
+++ Task04_Fast_Food/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task04_Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> orderQueue = new Queue<int>();
+            foreach (int elelement in orders)
+            {
+                orderQueue.Enqueue(elelement);
+            }
+
+            Console.WriteLine(orderQueue.Max());
+
+            while (true)
+            {
+                if(foodQuantity < orderQueue.Peek())
+                {
+                    break; 
+                }
+
+                foodQuantity -= orderQueue.Dequeue();
+
+                if (!orderQueue.Any())
+                {
+                    Console.WriteLine("Orders complete");
+                    break;
+                }
+            }
+
+            if (orderQueue.Any())
+            {
+                Console.Write("Orders left: ");
+                Console.WriteLine(String.Join(", ", orderQueue));
+            }
+            
+        }
+    }
+}
